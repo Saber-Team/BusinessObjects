9d4afe057c1198a15bbb6f97885a7d2e7fe202ce
eden <xxjinwei@outlook.com>
2016-04-13 17:04:31 +0800

    fix button & dropdown styles

--- dist/datepicker/datepicker.js
+++ dist/datepicker/datepicker.js
@@ -1,12 +1,13 @@
 define(['./pikaday'], function (Pikaday) {
     var defaults = {
-        theme    : 'zdh-datepicker',
-        format   : 'YYYY/MM/DD',
-        firstDay : 1,
+        theme     : 'zdh-datepicker',
+        format    : 'YYYY/MM/DD',
+        firstDay  : 1,
         // minDate  : new Date(2000, 0, 1),
         // maxDate  : new Date(2020, 12, 31),
-        yearRange: [2000, 2020],
-        i18n     : {
+        yearRange : [2000, 2020],
+        reposition: false,
+        i18n      : {
             previousMonth: 'Previous Month',
             nextMonth: 'Next Month',
             months: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(function (val) {
@@ -17,49 +18,55 @@ define(['./pikaday'], function (Pikaday) {
         }
     }
 
-    var activeClass = 'active';
+    var activeClass = 'active'
 
     function Datepicker(opt) {
-        $.extend(this.settings = {}, defaults, opt, true);
+        $.extend(this.settings = {}, defaults, opt, true)
         this.init()
     }
     $.extend(Datepicker.prototype, {
         init: function () {
-            this.initPicker();
+            this.initPicker()
         },
 
         initPicker: function () {
-            var me = this;
-            var settings = this.settings;
+            var me = this
+            var settings = this.settings
 
             // readonly
-            this.field  = $(settings.field);
-            this.field.prop('readonly', true);
+            this.field  = $(settings.field)
+            this.field.prop('readonly', true)
 
             // monkey patch
-            var originOnOpen  = settings.onOpen;
-            var originOnClose = settings.onClose;
+            var originOnOpen  = settings.onOpen
+            var originOnClose = settings.onClose
 
             settings.onOpen = function () {
-                me.toggleActive(true);
-                originOnOpen && originOnOpen.call(null);
+                me.toggleActive(true)
+                originOnOpen && originOnOpen.call(null)
             }
             settings.onClose = function () {
-                me.toggleActive(false);
-                originOnClose && originOnClose.call(null);
+                me.toggleActive(false)
+                originOnClose && originOnClose.call(null)
             }
 
             // start picker
-            this.picker = new Pikaday(settings);
+            this.picker = new Pikaday(settings)
+
+            Object.keys(Pikaday.prototype).forEach(function (handler) {
+                me[handler] = me.picker[handler].bind(me.picker)
+            })
+
+
         },
 
         toggleActive: function (isActive) {
-            this.active = isActive;
-            this.field.parent().toggleClass(activeClass, isActive);
+            this.active = isActive
+            this.field.parent().toggleClass(activeClass, isActive)
         },
 
         isActive: function () {
-            return this.active;
+            return this.active
         }
 
 
